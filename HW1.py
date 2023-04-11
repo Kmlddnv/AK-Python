2b570c2dd77e57282b7fc6b5c32208ae52d02b23
Kmlddnv <akmalkamoliddinov@gmail.com>
2023-04-11 16:52:59 +0500

    HW

---
+++ HW1.py
@@ -0,0 +1,81 @@
+# first_name = 'Khasan '
+# last_name = 'Karambayev '
+# weight = 90
+# name = first_name + last_name + str(weight)
+# print(name)
+
+'''Lesson 1, homework #1
+Please insert name: Khasan
+Привет, Khasan'''
+
+'''first_name = input('Введите свое имя: ')
+entry = 'Привет, '
+name = entry + first_name
+print(name)
+'''
+
+'''
+Lesson1, homework #2
+Function print
+print(50 + 20 + 30)
+
+Variables
+Num1 = 50
+Num2 = 20
+Num3 = 30
+print(Num1 + Num2 + Num3)
+
+User inserts
+Num1 = int(input('Введите первое число: '))
+Num2 = int(input('Введите второе число: '))
+Num3 = int(input('Введите третье число: '))
+print(Num1 + Num2 + Num3)'''
+
+'''''
+Lesson 1, homework #3
+n = int(input('количество школьников: '))
+k = int(input('количество яблок: '))
+x = k//n
+y = (k-(x*n))
+print(f'Каждому школьнику достанется по {x} шт яблок,\nв корзинке останется {y} шт яблок.')
+'''
+
+'''
+Lesson 1, homework #4
+Напишите программу, которая спрашивает число у пользователя и выводит следующее:
+Num1 = int(input('Введите число: '))
+print(f'Следующее число для {Num1} --> {Num1 + 1}\nПредыдущее число для {Num1} --> {Num1 - 1}')
+'''
+
+'''Lesson 1, homework #5
+print(''
+Twinkle, twinkle, little star,
+    How I wonder what you are!
+           Up above the world so high,
+           Like a diamond in the sky.
+ Twinkle, twinkle, little star,
+        How I wonder what you are!
+'')
+
+''''''
+Lesson 1, homework #6
+У меня есть 1000 долларов, поэтому я могу купить 3 футбольных мяча за 450 долларов 
+totalMoney = 1000
+quantity = 3
+price = 450
+a1 = ('У меня есть ')
+a2 = (' долларов')
+a3 = (', поэтому я могу купить ')
+a4 = (' футбольных мяча за ')
+name = a1 + str(totalMoney) + a2 + a3 + str(quantity) + a4 + str(price) + a2
+print(name)
+'''
+
+
+'''''
+Lesson 1, homework #7
+Создайте две переменных, со значением ‘spam’ и ‘eggs’, затем распечатайте объединив две переменной в одну целую
+a1 = 'spam'
+a2 = 'eggs'
+name = a1 + a2
+print(name)'''
